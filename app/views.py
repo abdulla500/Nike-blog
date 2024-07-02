cdd6943617ac7fa8744f4445363f59fbd42a4edf
Zoreyan <klovvergg@gmail.com>
2024-07-02 17:23:03 +0600

    post create

--- app/views.py
+++ app/views.py
@@ -7,27 +7,37 @@ def home(request):
     q = request.GET.get('q', '')
     posts = Post.objects.filter(title__icontains=q)
 
+    context = {
+        'posts': posts,
+        'q': q
+    }
 
-    return render(request, 'app/home.html', {'posts': posts, 'q': q})
+    return render(request, 'app/home.html', context)
 
 
 def post_detail(request, pk):
     post = Post.objects.get(id=pk)
 
-    return render(request, 'app/post_detail.html', {'post': post})
+    context = {
+        'post': post
+    }
+
+    return render(request, 'app/post_detail.html', context)
 
 
 def post_create(request):
     form = PostForm()
-
 
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
     
+    context = {
+        'form': form
+    }
 
-    return render(request, 'app/post_create.html')
+    return render(request, 'app/post_create.html', context)
 
 def post_update(request):
     return render(request, 'app/post_update.html')
